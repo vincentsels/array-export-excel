a47e2908e20b85dae20abb8e4b589519b1a86b2a
Vincent Sels <vincent_sels@hotmail.com>
2012-02-22 23:34:51 +0100

    New

---
+++ ArrayExportExcel/ArrayExportExcel.php
@@ -0,0 +1,22 @@
+<?php
+class ArrayExportExcelPlugin extends MantisPlugin {
+
+	function register() {
+		$this->name = 'Array export to Excel';
+		$this->description = 'This plugin allows to easily export PHP arrays to Excel.';
+
+		$this->version = '0.1';
+		$this->requires = array(
+			'MantisCore' => '1.2.0'
+		);
+
+		$this->author = 'Vincent Sels';
+		$this->contact = 'vincent_sels@hotmail.com';
+		$this->url = 'https://github.com/vincentsels';
+	}
+
+	function init() {
+		require_once( 'classes/ExcelExporter.php' );
+	}
+}
+?>
---
+++ README.md
@@ -0,0 +1,9 @@
+# Array export to Excel
+
+*Copyright (C) 2012 Vincent Sels*
+
+## Summary
+
+Array export to excel is a plugin for the open source [MantisBT](http://www.mantisbt.org) tool (also check out its [Github](https://github.com/mantisbt) page).
+The plugin does not add functionality to Mantis itself, but rather adds option for other plugins or code to easily export php arrays as excels.
+This plugin uses @oliverschwartz's [php-excel](https://github.com/oliverschwarz/php-excel) project.
