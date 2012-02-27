547957c9770eb7860dddc5eb3432745a181b052c
Vincent Sels <vincent_sels@hotmail.com>
2012-02-27 11:03:31 +0100

    Option to include header row in array export

--- ArrayExportExcel/ArrayExportExcel.php
+++ ArrayExportExcel/ArrayExportExcel.php
@@ -5,7 +5,7 @@ class ArrayExportExcelPlugin extends MantisPlugin {
 		$this->name = 'Array export to Excel';
 		$this->description = 'This plugin allows to easily export PHP arrays to Excel.';
 
-		$this->version = '0.1';
+		$this->version = '0.2';
 		$this->requires = array(
 			'MantisCore' => '1.2.0'
 		);
@@ -17,6 +17,7 @@ class ArrayExportExcelPlugin extends MantisPlugin {
 
 	function init() {
 		require_once( 'classes/ExcelExporter.php' );
+		require_once( 'classes/ExcelExporterIncludingHeader.php' );
 	}
 }
 ?>
---
+++ ArrayExportExcel/classes/ExcelExporterIncludingHeader.php
@@ -0,0 +1,35 @@
+<?php
+	/**
+	 * Class ExcelExporterIncludingHeader
+	 *
+	 * Extends ExcelExporter with header support.
+	 */
+class ExcelExporterIncludingHeader extends ExcelExporter
+{
+	/**
+	 * Add a worksheet and include a header row.
+	 * In case $p_header is supplied, use these values as header row.
+	 * In case no $p_header is supplied, use the keys of the array as header.
+	 *
+	 * @param string $p_title Title of worksheet
+	 * @param array $p_data 2-dimensional array of data
+	 * @param array $p_header optionally specify an array of values to use as header row
+	 */
+	public function addWorksheetWithHeader( $p_title, $p_data, $p_header = null )
+	{
+		if ( is_null( $p_header ) ) {
+			if ( is_array( $p_data ) && count( $p_data ) > 0 && count( $p_data[0] ) > 0 ) {
+				$p_header = array();
+				foreach ( $p_data[0] as $key => $value ) {
+					$p_header[] = $key;
+				}
+			}
+		}
+
+		if ( !is_null( $p_header ) ) {
+			array_unshift( &$p_data, $p_header );
+		}
+
+		parent::addWorkSheet( $p_title, $p_data );
+	}
+}
